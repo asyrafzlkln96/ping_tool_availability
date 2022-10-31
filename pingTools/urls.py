970bae09f8a6f1d9eaabf8de2e54829d3cd42afa
MOHD ASYRAF BIN ZULKALNAIN <53460015+asyrafzlkln96@users.noreply.github.com>
2022-11-01 00:58:29 +0800

    added urls for chart and report

--- pingTools/urls.py
+++ pingTools/urls.py
@@ -6,5 +6,8 @@ app_name = 'pingTools'
 
 urlpatterns = [
     path("pingtool", views.index, name="get_all_ping_status"),
-    path("update_switch_status", views.get_switch_status, name="update_switch_status")
+    path("update_switch_status", views.get_switch_status, name="update_switch_status"),
+    path("update_timestamp", views.convert_unix_timestamp_to_datetime, name="timestamp_conversion"),
+    path("create_chart", views.create_chart, name="generate_switch_status_chart"),
+    path("report", views.create_alert_report, name="create_alert_report")
 ]
