10cb9d6b1a3bfd554ec5d3dfe5235098f533b9d0
MOHD ASYRAF BIN ZULKALNAIN <53460015+asyrafzlkln96@users.noreply.github.com>
2022-11-01 00:57:26 +0800

    Update views.py

--- pingTools/views.py
+++ pingTools/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import F, When, Case, Q
+from datetime import datetime
+# import datetime
+import pandas as pd
+# import datetime as dt
 
 # Create your views here.
 
@@ -15,8 +19,34 @@ def get_switch_status(request):
     if request.method == 'GET':
         queryset = DataTerminals.objects.all().values()
         if queryset is not None:
+            timestamp = queryset.values_list('timestamp', flat=True)
             # Update switch status to 0 if all P1-P5 is 0
             val1 = queryset.filter(terminal_1__exact=0).filter(terminal_2__exact=0).filter(terminal_3__exact=0).filter(terminal_4__exact=0).filter(terminal_5__exact=0).update(switch_status=0)
             # Update switch status to 1 for other records
             val2 = queryset.filter(Q(terminal_1=1) | Q(terminal_2=1) | Q(terminal_3=1) | Q(terminal_4=1) | Q(terminal_5=1)).update(switch_status=1)
             return HttpResponse('<h1>Switch status updated!</h1>')
+
+def convert_unix_timestamp_to_datetime(request):
+    # To convert unix timestamp to datetime
+    df =  pd.DataFrame(list(DataTerminals.objects.all().values('timestamp')))
+    df2 = df.astype('int').astype("datetime64[s]")
+    data_dict = df2.to_dict()['timestamp']
+    bulk_update_list = []
+    for key, value in data_dict.items():
+        querys = DataTerminals.objects.get(id=key+1)
+        querys.date = value
+        bulk_update_list.append(querys)
+    DataTerminals.objects.bulk_update(bulk_update_list, ['date'])
+    return HttpResponse('<h1>Timestamp updated!</h1>')
+
+def create_chart(request):
+    pass
+                    
+def create_alert_report(request):
+    if request.method == 'GET':
+        queryset = DataTerminals.objects.all().values('switch','switch_status','date')
+        if queryset is not None:
+            queryset = queryset.filter(switch_status=0)
+            print('queryset:',queryset)
+            return HttpResponse('<h1>Alert report created!</h1>')
+
